cf04a9e278e491cfd981867a947451acd457f7e6
parallel5940 <parallel5940@neowiz.com>
2020-03-15 00:03:05 +0900

    odoc_20200314

--- JumpGame.java
+++ JumpGame.java
@@ -1,23 +1,29 @@
-public class JumpGame{
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class JumpGame {
     static int n;
-    static int board[][]= new int[100][100];
+    static int board[][] = new int[100][100];
     static int cache[][] = new int[100][100];
     static int ret = 0;
-    static int jump(int y, int x){
-        if(y >= n || x >= n){
+
+    static int jump(int y, int x) {
+        if (y >= n || x >= n) {
             return 0;
         }
-        if(y == n-1 && x == n-1){
+        if (y == n - 1 && x == n - 1) {
             return 1;
         }
-        if(cache[y][x] != -1){
+        if (cache[y][x] != -1) {
             return cache[y][x];
         }
         int jumpSize = board[y][x];
-        return cache[y][x] = jump(y+jumpSize, x) | jump(y, x+jumpSize);
+        return cache[y][x] = jump(y + jumpSize, x) | jump(y, x + jumpSize);
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int c = Integer.parseInt(bf.readLine());
         while(c-->0){
@@ -25,7 +31,7 @@ public class JumpGame{
             for(int i = 0; i < n; i++){
                 StringTokenizer st = new StringTokenizer(bf.readLine());
                 for(int j = 0; i < n; j++){
-                    board[i][j] = Integer.paseInt(st.nextToken());
+                    board[i][j] = Integer.parseInt(st.nextToken());
                     cache[i][j] = -1;
                 }
             }
