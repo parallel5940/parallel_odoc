5e56f0c915a9cd0fbfe1d9b3698b151c97ba7003
parallel5940 <parallel5940@neowiz.com>
2020-04-07 22:51:08 +0900

    odoc_20200407

--- BoardCover2.java
+++ BoardCover2.java
@@ -2,94 +2,127 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
-class BoardCover2 {
-    public static void main(final String[] args) throws NumberFormatException, IOException {
-        final BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(bf.readLine());
-        while (t-- > 0) {
-            StringTokenizer st = new StringTokenizer(bf.readLine());
-            final char[][] table = new char[Integer.parseInt(st.nextToken())][Integer.parseInt(st.nextToken())];
-            final int w = Integer.parseInt(st.nextToken());
-            final int h = Integer.parseInt(st.nextToken());
-            for (int i = 0; i < table.length; i++) {
-                st = new StringTokenizer(bf.readLine());
-                final char[] temp = st.nextToken().toCharArray();
-                for (int j = 0; j < table[i].length; j++) {
-                    table[i][j] = temp[j];
-                    Pair<i,j>
-                }
+public class BoardCover2 {
+    static boolean[][] board, block;
+    static List<List<int[]>> settingCase;
+    static int blockWidth, blockHeight, maxBlockCount;
 
-            }
-        }
-    }
-
-    public static void charArrayPrinter(final char[][] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                System.out.print(arr[i][j]);
-            }
-            System.out.println("");
-        }
-    }
-}
-
-class Block {
-    /**
-     * RotateMode 0:0 1:90 2:180 3:240 clockwise
-     */
-
-    private static int NUM_ROTATE_MODE = 4;
-    private final int blockBoardSize;
-
-    ArrayList<ArrayList<Pair<Integer, Integer>>> rotates = new ArrayList<>();
-
-    Block(final int height, final int width, final char[][] block) {
-        char[][] blockToRotate = block;
-        int offsetX = -1;
-        int offsetY = -1;
-        blockBoardSize = Math.max(width, height);
-        for (int i = 0; i < NUM_ROTATE_MODE; i++) {
-            rotates.add(new ArrayList<>());
-            for (int j = 0; j < blockBoardSize; j++) {
-                for (int k = 0; k < blockBoardSize; k++) {
-                    if (blockToRotate[j][k] == '#') {
-                        if (offsetX == -1) {
-                            offsetX = k;
-                            offsetY = j;
-                        }
-                        rotates.get(i).add(new Pair<>(j - offsetY, k - offsetX));
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int cases = Integer.parseInt(bufferedReader.readLine());
+        while (cases-- > 0) {
+            maxBlockCount = 0;
+            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine(), " ");
+            board = new boolean[Integer.parseInt(stringTokenizer.nextToken())][Integer
+                    .parseInt(stringTokenizer.nextToken())];
+            int blockWidth = Integer.parseInt(stringTokenizer.nextToken()),
+                    blockHeight = Integer.parseInt(stringTokenizer.nextToken()), blanckCount = 0;
+            for (int i = 0; i < board.length; i++) {
+                char row[] = bufferedReader.readLine().toCharArray();
+                for (int j = 0; j < board[i].length; j++) {
+                    board[i][j] = row[j] == '#';
+                    if (row[j] == '.') {
+                        blanckCount++;
                     }
                 }
             }
-            blockToRotate = rotateBlock(blockToRotate, blockBoardSize);
-            offsetX = -1;
-            offsetY = -1;
+            block = new boolean[blockWidth][blockHeight];
+            for (int i = 0; i < block.length; i++) {
+                char row[] = bufferedReader.readLine().toCharArray();
+                for (int j = 0; j < row.length; j++) {
+                    block[i][j] = row[j] == '#';
+                }
+            }
+            main.makeSettingCase();
+            main.countMaxBlock(0, 0, 0, blanckCount);
+            System.out.println(maxBlockCount);
         }
     }
 
-    private char[][] rotateBlock(final char[][] block, final int blockSize) {
-        final char[][] rotatedBlock = new char[blockSize][blockSize];
-        for (int i = 0; i < blockSize; i++) {
-            for (int j = 0; j < blockSize; j++) {
-                rotatedBlock[i][j] = block[blockSize - j - 1][i];
+    public void makeSettingCase() {
+        settingCase = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            List<int[]> setting = new ArrayList<>();
+            int leftTop[] = { -1, -1 };
+            for (int j = 0; j < block.length; j++) {
+                for (int k = 0; k < block[j].length; k++) {
+                    if (block[j][k]) {
+                        if (leftTop[0] == -1) {
+                            leftTop[0] = j;
+                            leftTop[1] = k;
+                        }
+                        setting.add(new int[] { j - leftTop[0], k - leftTop[1] });
+                    }
+                }
+            }
+            boolean isSame = !settingCase.isEmpty();
+            for (int j = 0; j < settingCase.size() && isSame; j++) {
+                for (int k = 0; k < setting.size() && isSame; k++) {
+                    isSame = isSame && settingCase.get(j).get(k)[0] == setting.get(k)[0]
+                            && settingCase.get(j).get(k)[1] == setting.get(k)[1];
+                }
+            }
+            if (!isSame)
+                settingCase.add(setting);
+            rotateBlock();
+        }
+    }
+
+    public void rotateBlock() {
+        boolean[][] rotatedBlock = new boolean[block[0].length][block.length];
+        for (int i = 0; i < rotatedBlock.length; i++) {
+            for (int j = 0; j < rotatedBlock[0].length; j++) {
+                rotatedBlock[i][j] = block[block.length - 1 - j][i];
             }
         }
-        return rotatedBlock;
-    }
-}
-
-class Pair<L, R> {
-    final L left;
-    final R right;
-
-    public Pair(L left, R right) {
-        this.left = left;
-        this.right = right;
+        block = rotatedBlock;
     }
 
-    static <L, R> Pair<L, R> of(L left, R right) {
-        return new Pair<L, R>(left, right);
+    public void countMaxBlock(int i, int j, int currentBlockCount, int remainedBlanckCount) {
+        if (currentBlockCount + remainedBlanckCount / settingCase.get(0).size() <= maxBlockCount)
+            return;
+        boolean cover = false;
+        for (; i < board.length && !cover; i++) {
+            j = 0;
+            for (; j < board[i].length && !cover; j++) {
+                if (!board[i][j]) {
+                    cover = true;
+                    for (List<int[]> settings : settingCase) {
+                        if (canSet(settings, i, j)) {
+                            setBlock(settings, i, j, true);
+                            countMaxBlock(i, j + 1, currentBlockCount + 1,
+                                    remainedBlanckCount - settingCase.get(0).size());
+                            setBlock(settings, i, j, false);
+                        }
+                    }
+                    board[i][j] = true;
+                    countMaxBlock(i, j + 1, currentBlockCount, remainedBlanckCount - 1);
+                    board[i][j] = false;
+                }
+            }
+        }
+        if (!cover)
+            maxBlockCount = Math.max(maxBlockCount, currentBlockCount);
+    }
+
+    private boolean canSet(List<int[]> settings, int cX, int cY) {
+        boolean canSet = true;
+        for (int i = 0; i < settings.size() && canSet; i++) {
+            int x = settings.get(i)[0];
+            int y = settings.get(i)[1];
+            canSet = canSet && cX + x >= 0 && cX + x < board.length && cY + y >= 0 && cY + y < board[0].length
+                    && !board[cX + x][cY + y];
+        }
+        return canSet;
+    }
+
+    private void setBlock(List<int[]> settings, int cX, int cY, boolean setMode) {
+        for (int[] set : settings) {
+            board[cX + set[0]][cY + set[1]] = setMode;
+        }
     }
 }
